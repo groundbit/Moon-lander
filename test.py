dbdf5d379321cb8cb855338d471da44f252f87cb
matthew <mattguarneros@gmail.com>
2016-11-29 15:48:34 -0800

    Add files via upload

--- test.py
+++ test.py
@@ -1,38 +1,49 @@
-#Moon lander
 # 1 - Import library
 import pygame
 from pygame.locals import *
 import sys
 import random
+import math
 
-BLACKISH = [10, 10, 10]
+BLACKISH = (10, 10, 10)
 WINDOW_WIDTH = 640
 WINDOW_HEIGHT = 480
 FRAMES_PER_SECOND = 30
+#global var for objects
+LANDER_MASS = 1
+LANDER_FUEL = 100
+PLANET_MASS = 7.347
+PLANET_RADIUS = 1.079
+GRAVITY = (9.8 * (PLANET_RADIUS ** 2))/ PLANET_MASS
+
+
+def getVelocity (oAltitude, velocity):
+    #landers alt = old alt + velocity + gravity
+    nAltitude = oAltitude + velocity + 2
+    return nAltitude
+    
 # 2 - Initialize pygame
 pygame.init()
-width = 640
-height = 480
+width = 648
+height = 460
+screen= pygame.display.set_mode([WINDOW_WIDTH, WINDOW_HEIGHT])
 MAX_WIDTH = width - 100
 MAX_HEIGHT = height - 100
 
-landerX = random.randrange(MAX_WIDTH)
-landerY = random.randrange(MAX_HEIGHT)
-landerRect = pygame.Rect(landerX, landerY, 100, 100)
+landerX = 0
+landerY = 0
+xSpeed = 0
+ySpeed = 0
+landerRect = pygame.Rect(landerX, landerY, 50, 50)
+#gravityCal =  (1 * 7.456)/(1079 + 1079)
 
-screen= pygame.display.set_mode([width, height])
-
-clock = pygame.time.Clock()
-framesPerSecond = 30
-
-#Gravity initalization
-gravity = 1
-xSpeed = 1
-ySpeed = 1
 # 3 - Load image(s)
-lander = pygame.image.load("images/ball.gif")
-lLeft = pygame.image.load("images/shiftR.png")
-lRight = pygame.image.load("images/shiftL.png")
+lander = pygame.image.load("images/lander.gif")
+landerRect =  lander.get_rect()
+
+
+clock = pygame.time.Clock()  # set the speed (frames per second)
+
  
 # 4 - Loop forever
 while True:
@@ -44,63 +55,58 @@ while True:
             # if it is quit the game
             pygame.quit()
             sys.exit()
-
-    
-
-        #Moving the lander
+                #Moving the lander
     keyPressedList = pygame.key.get_pressed()
      
     #--Moving Left       
     if keyPressedList[pygame.K_LEFT]:
-        landerX = landerX - 3
+        xSpeed = xSpeed - .5
+        
 
     #--Moving Right
     if keyPressedList[pygame.K_RIGHT]:
-        landerX = landerX + 3
+        xSpeed = xSpeed + .5
+        
 
     #--Moving Up
     if keyPressedList[pygame.K_UP]:
-        landerY = landerY - 3
+        ySpeed = ySpeed - .5
+        
+        
 
     #--Moving Down
     if keyPressedList[pygame.K_DOWN]:
-        landerY = landerY + 0
+        ySpeed = ySpeed + .5
         
-        # See if user clicked
-        if event.type == MOUSEBUTTONUP:
-            mouseX, mouseY = event.pos
-            
-            # Check if the click was in the rect of the lander, if so, move lander to random new spot
-            if landerRect.collidepoint( [mouseX, mouseY] ):
-                landerX = random.randrange(MAX_WIDTH)
-                landerY = random.randrange(MAX_HEIGHT)
-                landerRect = pygame.Rect(landerX, landerY, 100, 100)
+    landerX = landerX + xSpeed
+    landerY = landerY + ySpeed
+    landerY = getVelocity(landerY, ySpeed)
+
     
-   # 6 - clear the screen before drawing it again
+    
+    # 6 - clear the screen before drawing it again
     screen.fill(BLACKISH)
+    
+    
 
-    #Gravity simulation
-    if (landerRect.left < 0) or (landerRect.right > WINDOW_WIDTH):
-        xSpeed = -xSpeed
 
-    if (landerRect.top < 0) or (landerRect.bottom > WINDOW_HEIGHT):
-        ySpeed = gravity - ySpeed
+    # update the rectangle of the lander, based on the speed in two directions
+    #landerRect.left = landerRect.left + xSpeed + landerX
+    #landerRect.top = landerRect.top + ySpeed - landerY
 
-    # update the rectangle of the ball, based on the speed in two directions
-    #landerRect.left = landerRect.left + xSpeed
-    landerRect.top = landerRect.top + ySpeed
-    # 7 - draw the screen elements
-    screen.blit(lander, (landerX, landerY))
+    landerRect = pygame.Rect(landerX, landerY, 50, 50)
+    
+
+    #  Could alternatively be written as:      landerRect = landerRect.move(xSpeed, ySpeed)
+
+    #print landerRect
+    screen.blit(lander, landerRect)
+    
 
     # 8 - update the screen
     pygame.display.update()
 
     # 9 slow things down a bit
     clock.tick(FRAMES_PER_SECOND)  # make PyGame wait the correct amount
-
-
-
-
-
 
 
